77a306db224990c8915721224b60da2896e4652b
ivan1998iasnig@gmail.com <ivan1998iasnig@gmail.com>
2023-05-05 02:52:30 +0200

    ciao

--- Numpy/NumProva.py
+++ Numpy/NumProva.py
@@ -1,5 +1,5 @@
 import numpy as np
-
+"""
 a1 = np.array([1, 2, 3])     
 
 
@@ -40,4 +40,27 @@ print(a3.itemsize) #itemsize = 4
 
 print(a1.strides) #strides = (4,)
 print(a2.strides) #strides = (8,8)
+"""
 
+#CREATING ARRAYS
+"""
+sample_array = np.array([1, 2, 3])
+
+pippo = np.ones(sample_array, dtype=np.int64) #dtype facoltativo
+print(pippo) #array([1, 1, 1])
+
+pluto = np.zeros((4,5), dtype=np.int64)
+print(pluto) #[[0 0 0 0 0][0 0 0 0 0][0 0 0 0 0][0 0 0 0 0]]
+
+range_array = np.arange(10, 23, 4)
+print(range_array) #array([10, 14, 18, 22])
+
+random_array = np.random.randint(0, 10, size=(3, 5))
+print(random_array)
+
+random_array2 = np.random.random(size=(3, 5))
+print(random_array2)
+
+random_array3 = np.random.rand(5,3)
+print(random_array3)
+"""
