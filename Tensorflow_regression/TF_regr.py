aff43a42de0c25f4bfe394d1166df3d8a436618b
ivan1998iasnig@gmail.com <ivan1998iasnig@gmail.com>
2023-05-17 00:53:05 +0200

    ciao

--- Tensorflow_regression/TF_regr.py
+++ Tensorflow_regression/TF_regr.py
@@ -2,7 +2,7 @@
 import tensorflow as tf
 import numpy as np
 import matplotlib.pyplot as plt
-
+"""
 X = np.array([-7.0, -4.0, -1.0, 2.0, 5.0, 8.0, 11.0, 14.0]) # Create features
 
 y = np.array([3.0, 6.0, 9.0, 12.0, 15.0, 18.0, 21.0, 24.0]) # Create labels
@@ -13,4 +13,33 @@ plt.show()
 house_info = tf.constant(["bedroom", "bathroom", "garage"]) # Example input and output shapes of a regression model
 house_price = tf.constant([939700])
 print(house_info, house_price)
-     
+"""
+
+
+# Create features (using tensors)
+X = tf.constant([-7.0, -4.0, -1.0, 2.0, 5.0, 8.0, 11.0, 14.0])
+
+# Create labels (using tensors)
+y = tf.constant([3.0, 6.0, 9.0, 12.0, 15.0, 18.0, 21.0, 24.0])
+
+# Visualize it
+plt.scatter(X, y);
+plt.show();
+
+
+# Set random seed
+tf.random.set_seed(42)
+
+# Create a model using the Sequential API
+model = tf.keras.Sequential([
+  tf.keras.layers.Dense(1)
+])
+
+# Compile the model
+model.compile(loss=tf.keras.losses.mae, # mae is short for mean absolute error
+              optimizer=tf.keras.optimizers.SGD(), # SGD is short for stochastic gradient descent
+              metrics=["mae"])
+
+# Fit the model
+# model.fit(X, y, epochs=5) # this will break with TensorFlow 2.7.0+
+model.fit(tf.expand_dims(X, axis=-1), y, epochs=5)
