f836ac7e7741e936db255b68d31b9f564452e5ac
ivan1998iasnig@gmail.com <ivan1998iasnig@gmail.com>
2023-05-20 04:24:22 +0200

    ciao

--- Tensorflow_regression/TF_regr.py
+++ Tensorflow_regression/TF_regr.py
@@ -1,7 +1,6 @@
 #creating sample regression data
 import tensorflow as tf
-import numpy as np
-import matplotlib.pyplot as plt
+
 """
 X = np.array([-7.0, -4.0, -1.0, 2.0, 5.0, 8.0, 11.0, 14.0]) # Create features
 
@@ -14,21 +13,13 @@ house_info = tf.constant(["bedroom", "bathroom", "garage"]) # Example input and 
 house_price = tf.constant([939700])
 print(house_info, house_price)
 """
-
-
+#first regr AI
+"""
 # Create features (using tensors)
 X = tf.constant([-7.0, -4.0, -1.0, 2.0, 5.0, 8.0, 11.0, 14.0])
 
 # Create labels (using tensors)
 y = tf.constant([3.0, 6.0, 9.0, 12.0, 15.0, 18.0, 21.0, 24.0])
-
-# Visualize it
-plt.scatter(X, y);
-plt.show();
-
-
-# Set random seed
-tf.random.set_seed(42)
 
 # Create a model using the Sequential API
 model = tf.keras.Sequential([
@@ -41,5 +32,67 @@ model.compile(loss=tf.keras.losses.mae, # mae is short for mean absolute error
               metrics=["mae"])
 
 # Fit the model
-# model.fit(X, y, epochs=5) # this will break with TensorFlow 2.7.0+
-model.fit(tf.expand_dims(X, axis=-1), y, epochs=5)
+model.fit(tf.expand_dims(X, axis=-1), y, epochs=1000)
+
+# Make a prediction with the model
+output = model.predict([17.0])
+print(output)
+
+"""
+
+#Steps in improving a model with TensorFlow
+
+"""
+# Create features (using tensors)
+X = tf.constant([-7.0, -4.0, -1.0, 2.0, 5.0, 8.0, 11.0, 14.0])
+
+# Create labels (using tensors)
+y = tf.constant([3.0, 6.0, 9.0, 12.0, 15.0, 18.0, 21.0, 24.0])
+
+# Create the model 
+model = tf.keras.Sequential([
+  tf.keras.layers.Dense(100, activation="relu"),
+  tf.keras.layers.Dense(100, activation="relu"),
+  tf.keras.layers.Dense(100, activation="relu"),
+  tf.keras.layers.Dense(1)
+])
+
+# Compile the model
+model.compile(loss=tf.keras.losses.mae, # mae is short for mean absolute error
+              optimizer=tf.keras.optimizers.Adam(lr=0.0001), #changed optimizer
+              metrics=["mae"])
+
+# Fit the model (for longer)
+model.fit(tf.expand_dims(X, axis=-1), y, epochs=1000)
+
+# Make a prediction with the model
+output = model.predict([17.0])
+print(output)
+"""
+
+#Steps in improving a model with TensorFlow 2/3
+
+# Create features (using tensors)
+X = tf.constant([-7.0, -4.0, -1.0, 2.0, 5.0, 8.0, 11.0, 14.0])
+
+# Create labels (using tensors)
+y = tf.constant([3.0, 6.0, 9.0, 12.0, 15.0, 18.0, 21.0, 24.0])
+
+# Create the model 
+model = tf.keras.Sequential([
+  tf.keras.layers.Dense(50, activation=None),
+  tf.keras.layers.Dense(1)
+])
+
+# Compile the model
+model.compile(loss="mae", #same thing rewritten
+              optimizer=tf.keras.optimizers.Adam(lr=0.01), #changed optimizer
+              metrics=["mae"])
+
+# Fit the model (for longer)
+model.fit(tf.expand_dims(X, axis=-1), y, epochs=100)
+
+# Make a prediction with the model
+output = model.predict([17.0])
+print(output)
+
