e5fd92a670c2905300039b9dc8414bf354d51761
ivan1998iasnig@gmail.com <ivan1998iasnig@gmail.com>
2023-05-26 02:00:22 +0200

    ciao

--- Tensorflow_regression/TF_regr.py
+++ Tensorflow_regression/TF_regr.py
@@ -1,6 +1,7 @@
 #creating sample regression data
 import tensorflow as tf
 import matplotlib.pyplot as plt
+import pandas as pd
 
 """
 X = np.array([-7.0, -4.0, -1.0, 2.0, 5.0, 8.0, 11.0, 14.0]) # Create features
@@ -224,9 +225,8 @@ mse = tf.keras.losses.mse(y_true=y_test,
                                      y_pred=y_pred.squeeze())
 print(mse)
 """
-
 # Setting up TensorFlow modelling experiments part 1 (start with a simple model)
-
+"""
 X = tf.range(-100, 100, 4)
 
 y = X + 10
@@ -271,10 +271,9 @@ def mse(y_test, y_pred):
 mae_1 = mae(y_test, y_preds_1.squeeze()).numpy()
 mse_1 = mse(y_test, y_preds_1.squeeze()).numpy()
 print(mae_1, mse_1)
-
+"""
 # Setting up TensorFlow modelling experiments part 2 (increasing complexity)
-
-
+"""
 X = tf.range(-100, 100, 4)
 
 y = X + 10
@@ -317,13 +316,13 @@ def mse(y_test, y_pred):
                                        y_pred)
 
 # Calculate model_2 metrics
+
 mae_2 = mae(y_test, y_preds_1.squeeze()).numpy()
 mse_2 = mse(y_test, y_preds_1.squeeze()).numpy()
 print(mae_1, mse_1)
-
-
+"""
 # Comparing and tracking your TensorFlow modelling experiments
-
+"""
 import pandas as pd
 
 
@@ -333,10 +332,9 @@ model_results = [["model_1", mae_1, mse_1],
 
 all_results = pd.DataFrame(model_results, columns=["model", "mae", "mse"])
 print(all_results)
-
-
+"""
 # How to save a TensorFlow model
-
+"""
 model_2.save('prova_salvataggio_modello')
 
 model_2.save('prova_salvataggio_modello.h5') #modello salvato in hdf5 format
@@ -352,3 +350,78 @@ print(loaded_saved_model.summary()) #ovviamente i summary dei due modelli sono u
 model_2_preds = model_2.predict(X_test)
 saved_model_preds = loaded_saved_model.predict(X_test)
 print(mae(y_test, saved_model_preds.squeeze()).numpy() == mae(y_test, model_2_preds.squeeze()).numpy()) #questo pezzo di codice serve solo a vedere se l'import è corretto, ovviamente il modello e il modello salvato sono identici quindi abbiamo come risultato "true"
+"""
+# Putting together what we've learned part 1 (preparing a dataset)
+
+pd.set_option('display.max_columns', None) #per fare in modo che pandas mi mostri tutte le colonne e non mi mostri i puntini di sospensione 
+
+insurance = pd.read_csv("https://raw.githubusercontent.com/stedy/Machine-Learning-with-R-datasets/master/insurance.csv")
+  #print(insurance) #[1338 rows x 7 columns]
+
+insurance_one_hot = pd.get_dummies(insurance) #one hot encoding
+print(insurance_one_hot.head()) # view the converted columns
+
+
+# Create X & y values
+X = insurance_one_hot.drop("charges", axis=1)
+y = insurance_one_hot["charges"]
+
+
+# Create training and test sets
+from sklearn.model_selection import train_test_split
+
+X_train, X_test, y_train, y_test = train_test_split(X, 
+                                                    y, 
+                                                    test_size=0.2, 
+                                                    random_state=42) # set random state for reproducible splits
+     
+# View features
+print(X.head())
+
+# Set random seed
+tf.random.set_seed(42)
+
+# Add an extra layer and increase number of units
+insurance_model_2 = tf.keras.Sequential([
+  tf.keras.layers.Dense(100), # 100 units
+  tf.keras.layers.Dense(10), # 10 units
+  tf.keras.layers.Dense(1) # 1 unit (important for output layer)
+])
+
+# Compile the model
+insurance_model_2.compile(loss=tf.keras.losses.mae,
+                          optimizer=tf.keras.optimizers.Adam(), # Adam works but SGD doesn't 
+                          metrics=['mae'])
+
+# Fit the model and save the history (we can plot this)
+history = insurance_model_2.fit(X_train, y_train, epochs=115) #non serve riformattare X_train perchè pandas trasforma tutto in un array di numpy, quindi tensorflow sa già come gestirlo
+
+# Evaluate our larger model
+print(insurance_model_2.evaluate(X_test, y_test))
+
+#prima di creare il primo modello su github era stato costruito un primo modello che però aveva 7000 di mae come risultato
+#contando che la mediana è 9500 e la media 13300 (y_train.median() e y_train.mean()) il risultato era troppo sbagliato
+
+# Plot history (also known as a loss curve)
+pd.DataFrame(history.history).plot()
+plt.ylabel("loss")
+plt.xlabel("epochs");
+plt.show() #guardando la curva della loss mi sono reso conto che il modello scende velocemente fino ad un addestramento di 115 epoches, poi scende piano 
+
+# creiamo un nuovo dataframe con i dettagli del nuovo individuo
+person_x = pd.DataFrame({'age': [30],
+                         'bmi': [30],
+                         'children': [1],
+                         'sex_female': [1],
+                         'sex_male': [0],
+                         'smoker_no': [1],
+                         'smoker_yes': [0],
+                         'region_northeast': [1],
+                         'region_northwest': [0],
+                         'region_southeast': [0],
+                         'region_southwest': [0]})
+
+# usiamo il modello per fare la previsione
+prediction = insurance_model_2.predict(person_x)
+
+print("insurance cost: ", prediction[0][0])
